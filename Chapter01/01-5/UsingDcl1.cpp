c2f97f42607cd2148de545f3e21ec0d2a304621a
kbkim4 <ladages4@gmail.com>
2017-07-07 15:58:18 +0900

    test

---
+++ Chapter01/01-5/UsingDcl1.cpp
@@ -0,0 +1,15 @@
+/*
+#include <iostream>
+
+namespace Hybrid {
+	void HybFunc(void) {
+		std::cout<<"So Simple Function!"<<std::endl;
+		std::cout<<"In namespace Hybrid!"<<std::endl;
+	}
+}
+int main(void) {
+	using Hybrid::HybFunc;
+	HybFunc();
+	return 0;
+}
+*/
